ef161e812bdb2d5fbb2b3af9099030793593cf92
Choudhury Iqbal <ChoudhuryIqbal@users.noreply.github.com>
2020-10-22 23:51:32 -0400

    Primary File

---
+++ mobileFramework/src/test/java/com/appium/mobileFramework/sampleTest.java
@@ -0,0 +1,34 @@
+package com.appium.mobileFramework;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class sampleTest {
+	public static void main(String[] args) throws MalformedURLException {
+		AndroidDriver<AndroidElement> driver = Base.capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Hello");
+		// Option to hide keyboard
+		driver.hideKeyboard();
+		driver.findElementByXPath("//android.widget.RadioButton[@text='Male']").click();
+		driver.findElementById("android:id/text1").click();
+
+		String scrollToCountry = "new UiScrollable(new UiSelector()).scrollIntoView(text(\"Angola\"));";
+
+		driver.findElementByAndroidUIAutomator(scrollToCountry);
+		driver.findElementByXPath("//*[@text='Angola']").click();
+
+		driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
+		/// toast message
+		// android .widget.Toast[2]
+
+		String toastMessage = driver.findElement(By.xpath("//android.widget.Toast[1]")).getAttribute("name");
+		// name attibute for toast messages will have content.
+		System.out.println("Toast Mesaage is " + toastMessage);
+
+	}
+
+}
